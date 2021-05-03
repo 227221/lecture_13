a3d49d10ad444a33039f022b0602b84282232037
Adam Rohacek <xrohac05@vutbr.cz>
2021-05-03 10:56:12 +0200

    Add fibo fci

--- fibo.py
+++ fibo.py
@@ -1,6 +1,18 @@
+def recursive_ntb_fibo(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return recursive_ntb_fibo(n - 1) + recursive_ntb_fibo(n - 2)
+
 
 def main():
-    pass
+    fibo_number = int(input("Zadejte cislo: "))
+    fibo_sequence = []
+    for number in range(fibo_number + 1):
+        fibo_sequence.append(recursive_ntb_fibo(number))
+    print(fibo_sequence)
 
 
 if __name__ == '__main__':
